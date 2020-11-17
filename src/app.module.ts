a5fe665b6e448b829efe1e281bafc55ffb5be23d
Damian Obajtek <damian.obajtek@gmail.com>
2020-11-18 00:48:55 +0100

    added simple entity for task + migrations  + dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/task/task.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTaskDTO } from 'src/dto/create-task.dto';
+import { TaskDTO } from 'src/dto/task.dto';
+import { Task, TaskStatus } from 'src/entity/task.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TaskService {
+    constructor(@InjectRepository(Task) private taskRepository: Repository<Task>) {}
+
+    public async createOne(createTaskRequest: CreateTaskDTO) {
+        const task: Task = new Task();
+        task.title = createTaskRequest.title;
+        task.description = createTaskRequest.description;
+        task.status = TaskStatus.Created;
+
+        await this.taskRepository.save(task);
+
+        const taskDTO = new TaskDTO();
+        taskDTO.id = task.id;
+        taskDTO.title = task.title;
+        task.description = task.description;
+        taskDTO.status = task.status;
+
+        return taskDTO;
+    }
+}
