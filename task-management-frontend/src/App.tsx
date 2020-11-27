2dc3a6479e3b458cab89ef7113a73680bc24bb85
Damian Obajtek <damian.obajtek@gmail.com>
2020-11-27 13:19:08 +0100

    added simple fetching

---
+++ task-management-frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { TaskAPI } from './api/task.api';
+import { TaskDTO } from './api/dto/task.dto';
+
+function App() {
+  const [tasks, setTasks] = useState<TaskDTO[]>([])
+
+  useEffect(() => {
+     async function fetchAll() {
+       const resp = await TaskAPI.getAll();
+       setTasks(resp);
+     }
+     fetchAll();
+    }, [])
+
+  return (
+    <div className="App">
+      <ul>
+
+     {tasks.map(task => {
+         return <li>{task.title}</li>;
+       })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
